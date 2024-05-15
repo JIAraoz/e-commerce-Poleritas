7b84528a63d3d9f2de10624acf39f60cee2d0f41
MateoGiampietro <mateo.giampietro.10@gmail.com>
2024-05-15 19:42:43 -0300

    Nav, SearchBar y Profile

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Auth0Provider } from '@auth0/auth0-react'
+import { BrowserRouter } from 'react-router-dom'
 
 const domain = import.meta.env.VITE_APP_AUTH0_DOMAIN
 const clientId = import.meta.env.VITE_APP_AUTH0_CLIENT_ID
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Auth0Provider domain = {domain} clientId = {clientId} redirectUri = {window.location.origin} >
-      <App />
-    </Auth0Provider>
+    <BrowserRouter>
+      <Auth0Provider domain = {domain} clientId = {clientId} redirectUri = {window.location.origin} >
+        <App />
+      </Auth0Provider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
