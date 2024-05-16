d55c67648fd6e8279e24141e8d1f4b8b2fbe19db
Brayan8Holguin <raulhh1998@hotmail.com>
2024-05-16 01:50:11 -0600

    cambios solo en front
    ya agrege Eslint y prettier en la parte del cliente. pero aun no he logrado hacerlo en la parte de la api

--- Client/src/components/Cloudinary/Cloudinary.jsx
+++ Client/src/components/Cloudinary/Cloudinary.jsx
@@ -1,37 +1,40 @@
-import axios from "axios"
-import { useState } from "react"
+import axios from 'axios';
+import { useState } from 'react';
 
 export default function Cloudinary() {
-    const [Url, setUrl] = useState("")
+	const [Url, setUrl] = useState('');
 
-    const changeUploadImage = async (e) => {
-        const file = e.target.files[0]
+	const changeUploadImage = async (e) => {
+		const file = e.target.files[0];
 
-        const data = new FormData()
+		const data = new FormData();
 
-        data.append("file", file)
-        data.append("upload_preset", "Preset")
+		data.append('file', file);
+		data.append('upload_preset', 'Preset');
 
-        const response = await axios.post("https://api.cloudinary.com/v1_1/drq4immoc/image/upload", data)
+		const response = await axios.post(
+			'https://api.cloudinary.com/v1_1/drq4immoc/image/upload',
+			data,
+		);
 
-        setUrl(response.data.secure_url)
-        // console.log(response)
-    }
+		setUrl(response.data.secure_url);
+		// console.log(response)
+	};
 
-    const deleteImage = () => {
-        setUrl("")
-    }
+	const deleteImage = () => {
+		setUrl('');
+	};
 
-    return (
-        <div>
-            <p>Seleccionar imagen</p>
-            <input type='file' accept='image/*' onChange={changeUploadImage}/>
-            {Url && (
-                <div>
-                    <img src={Url}/>
-                    <button onClick={() => deleteImage()}>Eliminar imagen</button>
-                </div>
-            )}
-        </div>
-    )
+	return (
+		<div>
+			<p>Seleccionar imagen</p>
+			<input type='file' accept='image/*' onChange={changeUploadImage} />
+			{Url && (
+				<div>
+					<img src={Url} />
+					<button onClick={() => deleteImage()}>Eliminar imagen</button>
+				</div>
+			)}
+		</div>
+	);
 }
--- Client/src/components/Home/Home.jsx
+++ Client/src/components/Home/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
-    return (
-        <div>
-            <h2>Soy el Home</h2>
-        </div>
-    )
+	return (
+		<div>
+			<h2>Soy el Home</h2>
+		</div>
+	);
 }
