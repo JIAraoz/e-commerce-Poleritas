2676dda5acdc102fa7796d5608a72114f3ad8710
Nachvander <nachvander@gmail.com>
2024-05-16 18:29:49 -0300

    getFill

--- Api/src/controllers/getFill.js
+++ Api/src/controllers/getFill.js
@@ -1,8 +1,14 @@
+
 const {Article}=require('../db')
 
 const getFill = async(req, res) => {
     try {
-        const resultados = await Article.findAll();
+        const resultados = await Article.findAll({include:{
+            model: Category, 
+            as: 'Category',
+            attributes: ['id', 'name'],
+            through: { attributes: [] }
+        }});
 
         res.status(200).json({
             message: 'Datos obtenidos con exito',
