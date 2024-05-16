3534ce160b73e0f79095f8eaeed0f8bba7d3164f
Nachvander <nachvander@gmail.com>
2024-05-16 19:42:59 -0300

    paginacion/prueba

--- Api/src/controllers/getFill.js
+++ Api/src/controllers/getFill.js
@@ -1,21 +1,22 @@
-
+/* 
 const {Article,Category}=require('../db')
 
 const getFill = async(req, res) => {
     try {
+      
         const resultados = await Article.findAll({
             attributes: { exclude: ['createdAt', 'updatedAt']},
             include:{
             model: Category, 
-            
             attributes: ['categoryId', 'categoryName'],
             through: { attributes: [] }
-        }});
+        }})
+       
         if (resultados.length === 0) {
-            return res.status(400).json({ message: 'los datos no existen se deben crear' });
+            return res.status(404).json({ message: 'No sean encontrado elementos con esta característica' });
         }
         res.status(200).json({
-            message: 'Datos obtenidos con exito',
+            message: 'Datos obtenidos con éxito',
             resultados
         })
     } catch (error) {
@@ -23,5 +24,43 @@ const getFill = async(req, res) => {
         res.status(500).json({ message: 'Error interno del servidor' });
     }
 }
+
+module.exports = getFill; */
+const { Article, Category } = require('../db');
+
+const getFill = async (req, res) => {
+    try {
+        const pageSize = 5;
+        const page = req.query.page || 1; 
+
+        const totalCount = await Article.count(); 
+        const totalPages = Math.ceil(totalCount / pageSize);
+
+        const resultados = await Article.findAll({
+            attributes: { exclude: ['createdAt', 'updatedAt'] },
+            include: {
+                model: Category,
+                attributes: ['categoryId', 'categoryName'],
+                through: { attributes: [] }
+            },
+            offset: (page - 1) * pageSize, 
+            limit: pageSize 
+        });
+
+        if (resultados.length === 0) {
+            return res.status(404).json({ message: 'No se han encontrado elementos con esta característica' });
+        }
+
+        res.status(200).json({
+            message: 'Datos obtenidos con éxito',
+            totalPages,
+            currentPage: page,
+            resultados
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Error interno del servidor' });
+    }
+};
 
 module.exports = getFill;
