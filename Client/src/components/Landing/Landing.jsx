23f8c72d6cfdd27043070399236f97473cc303f6
Brayan8Holguin <raulhh1998@hotmail.com>
2024-05-16 02:07:02 -0600

    CARD
    agregue el archivo CARD y lo mostre en /home, puse un array de ejemplo ya que aun no tenemos productos reales, y ya con eso puedo Agregar CARDS

--- Client/src/components/Landing/Landing.jsx
+++ Client/src/components/Landing/Landing.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Landing() {
-    return (
-        <div className='landing'>
-            <Link to='/home'>
-                <button className="landing-button">Bienvenid@s a nuestra tienda!</button>
-            </Link>
-        </div>
-    )
+  return (
+    <div className="landing">
+      <Link to="/home">
+        <button className="landing-button">
+          Bienvenid@s a nuestra tienda!
+        </button>
+      </Link>
+    </div>
+  );
 }
