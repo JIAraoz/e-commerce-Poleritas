6cf95996e099a488bbceb098bd95da8f9661b0c0
MateoGiampietro <mateo.giampietro.10@gmail.com>
2024-05-18 14:27:07 -0300

    changes to form and nav

--- Client/src/components/Landing/Landing.jsx
+++ Client/src/components/Landing/Landing.jsx
@@ -5,7 +5,7 @@ export default function Landing() {
     return (
         <div className='landing'>
             <Link to='/home'>
-                <button className="landing-button">Bienvenid@s a nuestra tienda!</button>
+                <button className="landing-button">Bienvenid@s a nuestra tienda!!</button>
             </Link>
         </div>
     )
