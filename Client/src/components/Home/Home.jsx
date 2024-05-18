15dd370e45189eed15ea507eb330324e6d589378
SAldanaNizama <140188751+SAldanaNizama@users.noreply.github.com>
2024-05-18 15:29:41 -0500

    estilos de la landing con el home
    hice unos pequeños cambios en css de la landing y del home

--- Client/src/components/Home/Home.jsx
+++ Client/src/components/Home/Home.jsx
@@ -1,3 +1,52 @@
+// import { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import Nav from '../Nav/Nav';
+// import './Home.css';
+// import Cards from '../Cards/Cards';
+// import Pagination from '../pagination/Pagination';
+
+// export default function Home() {
+//   const [products, setProducts] = useState([]);
+//   const [loading, setLoading] = useState(true);
+//   const [currentPage, setCurrentPage] = useState(1);
+//   const [totalPages, setTotalPages] = useState(0);
+//   const productsPerPage = 5;
+
+//   useEffect(() => {
+//     async function fetchProducts(page) {
+//       setLoading(true);
+//       try {
+//         const response = await axios.get(`https://e-commerce-grupo03.onrender.com/articles?page=${page}&limit=${productsPerPage}`);
+//         setProducts(response.data.result);
+//         setTotalPages(response.data.totalPages);
+//         setCurrentPage(response.data.currentPage);
+//         setLoading(false);
+//       } catch (error) {
+//         console.error('Error fetching the products:', error);
+//         setLoading(false);
+//       }
+//     }
+
+//     fetchProducts(currentPage);
+//   }, [currentPage]);
+
+//   const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+//   return (
+//     <div className="home">
+//       <Nav />
+//       <Cards products={products} loading={loading} />
+//       <Pagination
+//         productsPerPage={productsPerPage}
+//         totalPages={totalPages}
+//         paginate={paginate}
+//         currentPage={currentPage}
+//       />
+//     </div>
+//   );
+// }
+
+
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Nav from '../Nav/Nav';
@@ -26,7 +75,6 @@ export default function Home() {
         setLoading(false);
       }
     }
-
     fetchProducts(currentPage);
   }, [currentPage]);
 
@@ -34,14 +82,13 @@ export default function Home() {
 
   return (
     <div className="home">
-      <Nav />
-      <Cards products={products} loading={loading} />
-      <Pagination 
-        productsPerPage={productsPerPage} 
-        totalPages={totalPages} 
-        paginate={paginate} 
-        currentPage={currentPage}
-      />
+      <div className="home-background">
+        <Nav />
+        <div className="home-content">
+          <Cards products={products} loading={loading} />
+          <Pagination productsPerPage={productsPerPage} totalPages={totalPages} paginate={paginate} currentPage={currentPage} />
+        </div>
+      </div>
     </div>
   );
 }
--- Client/src/components/Landing/Landing.jsx
+++ Client/src/components/Landing/Landing.jsx
@@ -1,13 +1,32 @@
+// import Logout from '../Auth0/Logout/Logout';
+// import Login from '../Auth0/Login/Login';
+// import { useAuth0 } from '@auth0/auth0-react';
+// import "./Landing.css"
+// export default function Landing() {
+//   const { isAuthenticated} = useAuth0();
+//   return (
+//     <div  className="landing">
+//       {isAuthenticated ? <Logout /> : <Login />}
+//     </div>
+//   );
+
+// }
+
 import Logout from '../Auth0/Logout/Logout';
 import Login from '../Auth0/Login/Login';
 import { useAuth0 } from '@auth0/auth0-react';
 import "./Landing.css"
+
 export default function Landing() {
-  const { isAuthenticated} = useAuth0();
+  const { isAuthenticated } = useAuth0();
+
   return (
-    <div  className="landing">
-      {isAuthenticated ? <Logout /> : <Login />}
+    <div className="landing">
+      <div className="landing-background">
+        <div className="landing-content">
+          {isAuthenticated ? <Logout /> : <Login />}
+        </div>
+      </div>
     </div>
   );
-
 }
