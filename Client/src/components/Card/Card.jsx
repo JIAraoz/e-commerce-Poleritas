97301fb17e8be94afb26b3e65cccd8d0cfa7f59b
SAldanaNizama <140188751+SAldanaNizama@users.noreply.github.com>
2024-05-18 15:53:08 -0500

    estilo de card actualizada

--- Client/src/components/Card/Card.jsx
+++ Client/src/components/Card/Card.jsx
@@ -1,22 +1,16 @@
 /* eslint-disable react/prop-types */
-
-
 import './card.css';
 
-export default function Card({ title, image, price}) {
+export default function Card({ title, image, price }) {
   return (
-    
     <div className="card">
-      <div className="image-container" >
-      <img src={image} alt={title} />
-
+      <div className="card-image-container">
+        <img src={image} alt={title} />
       </div>
-      <h2>{title}</h2>
-     
-      <p>{`Price: $${price}`}</p>
-    
+      <div className="card-content">
+        <h2>{title}</h2>
+        <p className="card-price">${price}</p>
+      </div>
     </div>
   );
 }
-
-
