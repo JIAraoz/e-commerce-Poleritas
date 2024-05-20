5bd925d903d8779a5c474b27ffc39bca2ad5f56d
SAldanaNizama <140188751+SAldanaNizama@users.noreply.github.com>
2024-05-19 19:33:48 -0500

    estilo del formulario

---
+++ Client/src/components/Footer/Footer.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+	faFacebook,
+	faTwitter,
+	faInstagram,
+} from '@fortawesome/free-brands-svg-icons';
+
+const Footer = () => (
+	<footer className='footer-content'>
+		<div className='social-buttons'>
+			<a
+				href='https://www.facebook.com'
+				target='_blank'
+				rel='noopener noreferrer'
+			>
+				<button>
+					<FontAwesomeIcon icon={faFacebook} />
+				</button>
+			</a>
+			<a
+				href='https://www.twitter.com'
+				target='_blank'
+				rel='noopener noreferrer'
+			>
+				<button>
+					<FontAwesomeIcon icon={faTwitter} />
+				</button>
+			</a>
+			<a
+				href='https://www.instagram.com'
+				target='_blank'
+				rel='noopener noreferrer'
+			>
+				<button>
+					<FontAwesomeIcon icon={faInstagram} />
+				</button>
+			</a>
+		</div>
+		<p>© 2022 Nuestro Sitio Web. Todos los derechos reservados.</p>
+	</footer>
+);
+
+export default Footer;
