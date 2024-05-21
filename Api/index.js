d9bf0639e3cdbca266a4395719e0b8da59635ecb
Nachvander <nachvander@gmail.com>
2024-05-21 19:27:02 -0300

    resetDB

--- Api/index.js
+++ Api/index.js
@@ -1,7 +1,7 @@
 const server = require("./src/server.js");
 const { conn } = require("./src/db.js");
 
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   console.log("Conexion con base de datos con exito");
   server.listen(3001, () => {
     console.log("Servidor levantado con exito");
