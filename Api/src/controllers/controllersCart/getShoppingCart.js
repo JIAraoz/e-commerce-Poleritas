6770a040633ac6f0d2bb6379db80369e76d8a22b
Nachvander <nachvander@gmail.com>
2024-05-23 20:27:12 -0300

    test01

--- Api/src/controllers/controllersCart/getShoppingCart.js
+++ Api/src/controllers/controllersCart/getShoppingCart.js
@@ -1,3 +1,26 @@
-const getShoppingCart=async()=>{
+const {User}=require('../../db')
+
+const getShoppingCart=async(req,res)=>{
+    try {
+        const id=req.query.id
+        if(!id) return res.status(400).json({message:'Faltan datos o son inválidos en el cuerpo de la solicitud'})
+        const user=await User.findOne({
+        where:{
+        userId:id
+        },
+        include: [{
+            model: ShoppingCart,
+            where:{
+                cartStatus:"Active"
+            }
+        }]
+
+        })
+        console.log(user);
+        res.json({user})        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Error interno del servidor'});
+    }
     
 }
