918433c782a1977ab92d6f90981963b5c8a7c38a
aljodavid10 <aljodavid100@gmail.com>
2024-05-23 11:44:37 -0500

    feature getUsers

--- Api/src/routes/detailRoutes/routerUser.js
+++ Api/src/routes/detailRoutes/routerUser.js
@@ -1,5 +1,9 @@
 const routerUser = require("express").Router();
 
-routerUser.get('list-users');
+const getUser = require("../../controllers/controllersUser/getUsers");
+const postUser=require('../../controllers/controllersUser/postUser')
+
+routerUser.get('/list-users', getUser);
+routerUser.post('/postUser',postUser)
 
 module.exports = routerUser;
