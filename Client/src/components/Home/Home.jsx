ecf5b26b5831013229ae5a5d9d54763d50ced12c
SAldanaNizama <140188751+SAldanaNizama@users.noreply.github.com>
2024-05-23 10:03:24 -0500

    Estilos del home
    el estilo del home esta, falta darle las funcionalidades

--- Client/src/components/Home/Home.jsx
+++ Client/src/components/Home/Home.jsx
@@ -1,113 +1,74 @@
 import { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { updateQuery } from '../../redux/actions';
 import axios from 'axios';
 import './Home.css';
-import Cards from '../Cards/Cards';
-import Pagination from '../pagination/Pagination';
 import Footer from '../Footer/Footer';
 
 export default function Home() {
-	const [products, setProducts] = useState([]);
-	const [loading, setLoading] = useState(true);
-	const [currentPage, setCurrentPage] = useState(1);
-	const [totalPages, setTotalPages] = useState(0);
-	const [categories, setCategories] = useState([]);
-	const [order, setOrder] = useState('');
-	const [category, setCategory] = useState('');
+ const [categories, setCategories] = useState([]);
 
-	const dispatch = useDispatch();
+ useEffect(() => {
+   async function fetchCategories() {
+     try {
+       const response = await axios.get(
+         'https://e-commerce-grupo03.onrender.com/getCategory'
+       );
+       setCategories(response.data.result);
+     } catch (error) {
+       alert(error.message);
+     }
+   }
 
-	const query = useSelector((state) => state.query);
+   fetchCategories();
+ }, []);
 
-	const productsPerPage = 5;
-
-	useEffect(() => {
-		async function fetchProducts(page) {
-			setLoading(true);
-			try {
-				const response = await axios.get(
-					`https://e-commerce-grupo03.onrender.com/articles?page=${page}&limit=${productsPerPage}&category=${query.filter}&order=${query.order}&name=${query.search}`,
-				);
-				setProducts(response.data.result);
-				setTotalPages(response.data.totalPages);
-				setCurrentPage(response.data.currentPage);
-				setLoading(false);
-			} catch (error) {
-				console.error('Error fetching the products:', error);
-				setLoading(false);
-			}
-		}
-		async function fetchCategories() {
-			try {
-				const response = await axios.get(
-					'https://e-commerce-grupo03.onrender.com/getCategory',
-				);
-				setCategories(response.data.result);
-			} catch (error) {
-				alert(error.message);
-			}
-		}
-		fetchCategories();
-		fetchProducts(currentPage);
-	}, [currentPage, query]);
-
-	const handlerOrder = (event) => {
-		setOrder(event.target.value);
-	};
-
-	const handleCategory = (event) => {
-		setCategory(event.target.value);
-	};
-
-	const handleFilters = () => {
-		query.order = order;
-		query.filter = category;
-		setCurrentPage(1);
-		dispatch(updateQuery(query));
-	};
-
-	const paginate = (pageNumber) => setCurrentPage(pageNumber);
-
-	return (
-		<div className='home'>
-			<div className='home-background'>
-				<div className='filters'>
-					<div className='custom-select'>
-						<select name='order' defaultValue='order' onChange={handlerOrder}>
-							<option value='order' disabled='disabled'>
-								Order
-							</option>
-							<option value='A-Z'>A-Z</option>
-							<option value='Z-A'>Z-A</option>
-							<option value='price-asc'>^ price</option>
-							<option value='price-desc'>v Price</option>
-						</select>
-					</div>
-					<div className='custom-select'>
-						<select name='Category' defaultValue='' onChange={handleCategory}>
-							<option value=''>All</option>
-							{categories.map((category, index) => (
-								<option key={index} value={category.categoryId}>
-									{category.categoryName}
-								</option>
-							))}
-						</select>
-					</div>
-					<button onClick={handleFilters}>Apply filters</button>
-				</div>
-
-				<div className='home-content'>
-					<Cards products={products} loading={loading} />
-					<Pagination
-						productsPerPage={productsPerPage}
-						totalPages={totalPages}
-						paginate={paginate}
-						currentPage={currentPage}
-					/>
-				</div>
-			</div>
-			<Footer />
-		</div>
-	);
+ return (
+   <div className='home'>
+     <div className='home-background'>
+       <div className='categories'>
+         <div>
+           <img className='image' src='../../public/camisetaMasVendida.jpg' alt='Men' />
+           <h3>Men</h3>
+         </div>
+         <div>
+           <img className='image' src='../../public/CamisetaDeportivaDeMujer.jpg' alt='Womens' />
+           <h3>Womens</h3>
+         </div>
+         <div>
+           <img className='image' src='../../public/Polounisex-2.jpg' alt='Unise' />
+           <h3>Unisex</h3>
+         </div>
+       </div>
+       <div className='featured-products'>
+         <h2>Featured Products</h2>
+         <div className='featured-carousel'>
+           {/* componente carrusel para feature products*/}
+         </div>
+       </div>
+       <div className='new-products'>
+         <h2>New Products</h2>
+         <div className='new-carousel'>
+           {/* componente carrusel para new product */}
+         </div>
+       </div>
+     </div>
+     <div className='free-shipping'>
+       <div className='free-shipping-content'>
+         <h2>Free Shipping</h2>
+         <p>
+           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+           ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+           aliquip ex ea commodo consequat.
+         </p>
+       </div>
+       <div className='delivery-truck'>
+         <div className=''>
+           <img className='carrito' src="../../public/gratis.png" alt="" />
+           <img className='carrito' src="../../public/carrito.png" alt="" />
+         </div>
+       </div>
+     </div>
+     <Footer />
+   </div>
+ );
 }
