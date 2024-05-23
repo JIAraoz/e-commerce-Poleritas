27ebbfe6653a60023b5c4964ab269a0f5b1c9360
Nachvander <nachvander@gmail.com>
2024-05-23 20:33:36 -0300

    test02

--- Api/src/routes/detailRoutes/routerCart.js
+++ Api/src/routes/detailRoutes/routerCart.js
@@ -1,3 +1,4 @@
+const getShoppingCart=require('../../controllers/controllersCart/getShoppingCart')
 const routerCart = require("express").Router();
-
+routerCart.get('/getShoppingCart',getShoppingCart)
 module.exports = routerCart;
