95a1b7a62af3c001c90b458baa9823c60387e690
Nachvander <nachvander@gmail.com>
2024-05-24 09:52:41 -0300

    shoppingcartFix2

--- Api/src/controllers/controllersCart/getShoppingCart.js
+++ Api/src/controllers/controllersCart/getShoppingCart.js
@@ -58,7 +58,7 @@ const getShoppingCart = async (req, res) => {
             include: { model: ShoppingCart }
         });
 
-        if (userCard === null) {
+        if (userCard === null) {console.log('entre al if de la linea 61');
             // Si no se encuentra el usuario con carrito, buscar el usuario sin carrito
             const user = await User.findOne({ where: { userId: id } });
             if (user === null) {
