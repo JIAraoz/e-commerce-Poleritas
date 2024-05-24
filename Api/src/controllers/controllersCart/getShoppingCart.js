2c048ffac30743ef5b1f63aa5c5bbf560de181c6
Nachvander <nachvander@gmail.com>
2024-05-24 00:16:19 -0300

    feat-create-shoppingcart5

--- Api/src/controllers/controllersCart/getShoppingCart.js
+++ Api/src/controllers/controllersCart/getShoppingCart.js
@@ -4,7 +4,7 @@ const getShoppingCart=async(req,res)=>{
     try {
         const id=req.query.id
         if(!id) return res.status(400).json({message:'Faltan datos o son inválidos en el cuerpo de la solicitud'})
-        const user=await User.findOne({
+        const userCard=await User.findOne({
         where:{
         userId:id
         },
@@ -14,9 +14,27 @@ const getShoppingCart=async(req,res)=>{
         }
 
         })
-
-        console.log(user);
-        res.json({user})        
+        if(userCard===null){
+            const user=await findOne({where:{
+                userId:id
+            }})
+            if (user===null) {
+                return res.status(400).json({message:'No se encontro el usuario'})
+            }else{
+                const shoppingCart=await ShoppingCart.create({
+                    cartSubtotal:0,
+                    cartPayment:"None",
+                    isActive:true
+                })
+                await user.addShoppingCart(shoppingCart)
+                const updatedUser = await User.findOne({
+                    where: { userId: id },
+                    include: { model: ShoppingCart }
+                });
+                res.status(200).json({result:updatedUser})
+            }
+            
+        }      
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: 'Error interno del servidor'});
