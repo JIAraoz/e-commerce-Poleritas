a07d418ba7ff94b01b260fcd816ff804f1f3c0af
MateoGiampietro <mateo.giampietro.10@gmail.com>
2024-05-23 22:07:46 -0300

    add desactivate Shopping Cart

---
+++ Api/src/controllers/controllersCart/desactivateShoppingCart.js
@@ -0,0 +1,22 @@
+const {User,ShoppingCart}=require('../../db');
+
+
+const desactivateShoppingCart=async(req,res)=>{
+    try {
+        const cartId = req.query.cartId
+        if (cartId) {
+            const cartToDesactivate=await ShoppingCart.findByPk(cartId)
+            if (cartToDesactivate) {
+                cartToDesactivate.isActive = false
+            }
+            return res.status(200).json({message:'Carrito pagado con éxito'})
+        } else {
+            return res.status(400).json({message:'Faltan datos o son inválidos en el cuerpo de la solicitud'})
+        }
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Error interno del servidor'});
+    }
+}
+module.exports=desactivateShoppingCart
