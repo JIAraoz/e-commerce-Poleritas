ab34831d8421b8c5a2f4c297a2672451326d23bc
Brayan8Holguin <raulhh1998@hotmail.com>
2024-05-25 17:34:23 -0600

    fix-style
    ya volvi a agregar los estilos, asi que quedo reparado el problema de versiones

--- Client/src/components/Detail/Detail.jsx
+++ Client/src/components/Detail/Detail.jsx
@@ -1,66 +1,64 @@
 // Detail.jsx
-import { useAuth0 } from '@auth0/auth0-react';
-import axios from 'axios'
+import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import './Detail.css';
 
 import { useEffect, useState } from 'react';
 
-export default function Detail() {
+export default function Detail({ allProducts, setAllProducts }) {
 	const { id } = useParams();
-	const [product,setProduct]=useState()
-	const { user } = useAuth0();
-
-	useEffect(()=>{
-		async function getProduct(){
-			const {data}=await axios.get(`https://e-commerce-grupo03.onrender.com/article/detail/${id}`)
-			setProduct(data)
+	const [product, setProduct] = useState();
+	useEffect(() => {
+		async function getProduct() {
+			const { data } = await axios.get(
+				`https://e-commerce-grupo03.onrender.com/article/detail/${id}`,
+			);
+			setProduct(data);
 		}
-		getProduct()
-	},[id])
-
+		getProduct();
+	}, [id]);
 
 	if (!product) {
 		return <div>Producto no encontrado</div>;
 	}
 
-	const handleAddToCart = async () => {
-		try {
-			var userResponse = await axios.get(
-				`https://e-commerce-grupo03.onrender.com/user/user_email?email=${user.email}`
-			);
-
-			if (userResponse.data.result.userId) {
-				var cartResponse = await axios.get(`https://e-commerce-grupo03.onrender.com/cart/getShoppingCart?id=${userResponse.data.result.userId}`);
-				console.log(cartResponse.data.result);
-			}
-
-			var addArticleResponse = await axios.get(`https://e-commerce-grupo03.onrender.com/cart/add_article_cart?cartid=${cartResponse.data.result[0].cartId}&articleid=${id}&quantity=${1}`)
-			if (addArticleResponse) {
-				alert("Tu producto ha sido agregado al carrito")
-			}
-		} catch (error) {
-			console.error('Ha ocurrido un error: ' + error.message);
-		}
-	}
-
 	return (
 		<div>
+			<button id='back-button'>Back</button>
 			<div className='detail-container'>
-				<div className='detail'>
-					<h2>ID: {product.articleId}</h2>
-					<h4>Nombre: {product.articleName}</h4>
-					<h4>Descripción: {product.articleDescription}</h4>
-					<h4>Precio: {product.articlePrice}</h4>
-					<h4>Stock: {product.articleStock}</h4>
-					<h4>Categoría: {product.categories[0].categoryName}</h4>
-					<button onClick={() => handleAddToCart()}>Añadir al carrito</button>
-				</div>
 				<div className='photo-container'>
 					<img src={product.articleImage} alt={product.articleName} />
 				</div>
+				<div className='detail'>
+					<h1 className='name'>{product.articleName}</h1>
+					<h4 className='price'>${product.articlePrice}</h4>
+					<div className='pay'>
+						<div className='size-options'>
+							<p>Size:</p>
+							<select>
+								<option value='xs'>XS</option>
+								<option value='s'>S</option>
+								<option value='m'>M</option>
+								<option value='l'>L</option>
+							</select>
+						</div>
+						<div className='payment-methods'>
+							<p>Payment Methods</p>
+							<img src='/Pagos.png' alt='pay' />
+						</div>
+					</div>
+					<p className='stock'>Stock: {product.articleStock} pcs</p>
+					<div className='cart-container'>
+						<button className='add-to-cart'>Add to Cart</button>
+						<p className='free-shipping'>Free Shipping!!</p>
+					</div>
+				</div>
 			</div>
-
+			<div className='description-container'>
+				<h1>Description:</h1>
+				<h4>{product.articleDescription}</h4>
+				<h4>Category: {product.categories[0].categoryName}</h4>
+			</div>
 		</div>
 	);
 }
