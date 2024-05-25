099fee5fefb70c44041d712ad8c8432100f7a86c
SAldanaNizama <140188751+SAldanaNizama@users.noreply.github.com>
2024-05-24 19:03:00 -0500

    fix-searchbar
    cofiguracion del buscador, ahora podes buscar con la tecla enter y se deja lo que estas buscando

--- Client/src/components/SearchBar/SearchBar.jsx
+++ Client/src/components/SearchBar/SearchBar.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, Link } from 'react-router-dom';
 import { updateQuery } from '../../redux/actions';
@@ -7,43 +7,49 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 export default function SearchBar() {
-	const [name, setName] = useState('');
+  const [name, setName] = useState('');
+  const navigate = useNavigate();
+  const [showMessage, setShowMessage] = useState(false);
+  const dispatch = useDispatch();
+  const query = useSelector((state) => state.query);
 
-	const navigate = useNavigate();
+  useEffect(() => {
+    setName(query.search || '');
+  }, [query.search]);
 
-	const [showMessage, setShowMessage] = useState(false);
+  const handleChange = (event) => {
+    setName(event.target.value);
+  };
 
-	const dispatch = useDispatch();
-	const query = useSelector((state) => state.query);
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      handleSearch();
+    }
+  };
 
-	const handleChange = (event) => {
-		setName(event.target.value);
-	};
+  const handleSearch = () => {
+    query.search = name;
+    dispatch(updateQuery(query));
+  };
 
-	const handleClick = (event) => {
-		event.preventDefault();
-		query.search = name;
-		dispatch(updateQuery(query));
-		setName('');
-	};
-
-	return (
-		<div className='search-bar'>
-			<input
-				type='search'
-				onChange={handleChange}
-				value={name}
-				placeholder='Buscar...'
-			/>
-			<Link to='/home'>
-				<button onClick={handleClick}>
-					<FontAwesomeIcon icon={faSearch} />
-				</button>
-			</Link>
-			{showMessage && (
-				<div className='no-results'>No se encontraron resultados.</div>
-			)}
-			<div className='search-results'></div>
-		</div>
-	);
+  return (
+    <div className='search-bar'>
+      <input
+        type='search'
+        onChange={handleChange}
+        value={name}
+        placeholder='Buscar...'
+        onKeyDown={handleKeyDown}
+      />
+      <Link to='/home'>
+        <button onClick={handleSearch}>
+          <FontAwesomeIcon icon={faSearch} />
+        </button>
+      </Link>
+      {showMessage && (
+        <div className='no-results'>No se encontraron resultados.</div>
+      )}
+      <div className='search-results'></div>
+    </div>
+  );
 }
