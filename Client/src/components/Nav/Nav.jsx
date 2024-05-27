fd747c528df4eb9c115973297d9e2f83ad444210
Brayan8Holguin <raulhh1998@hotmail.com>
2024-05-26 19:56:43 -0600

    Logo Navbar
    se agrego el logo a navbar

--- Client/src/components/Nav/Nav.jsx
+++ Client/src/components/Nav/Nav.jsx
@@ -12,6 +12,7 @@ function Nav() {
 	return (
 		<div className='nav'>
 			<div className='left'>
+				<img src='/Poleritas.png' alt='Poleritas' className='Logo-NavBar' />
 				<Link to='/home'>
 					<button>HOME</button>
 				</Link>
