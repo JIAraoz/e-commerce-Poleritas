713ad3e3b7625a70ec78cd7799366a0cc32a6e45
Brayan8Holguin <raulhh1998@hotmail.com>
2024-05-26 21:10:10 -0600

    style-Cart
    ya agregue estilo, deje puesto el total, para que haga la suma y de ahí muestre el total de la compra, pero aun no esta funcionando, para que lo tomen en cuenta

--- Client/src/components/Cart/Cart.jsx
+++ Client/src/components/Cart/Cart.jsx
@@ -2,103 +2,124 @@ import { useAuth0 } from '@auth0/auth0-react';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import './Cart.css';
 export default function Cart() {
-    const { user } = useAuth0();
-    const [userData, setUserData] = useState({});
-    const [cartItems, setCartItems] = useState([]);
-    const [cartResponse, setCartResponse] = useState(null);
-    const navigate = useNavigate();
+	const { user } = useAuth0();
+	const [userData, setUserData] = useState({});
+	const [cartItems, setCartItems] = useState([]);
+	const [cartResponse, setCartResponse] = useState(null);
+	const navigate = useNavigate();
 
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const userResponse = await axios.get(
-                    `https://e-commerce-grupo03.onrender.com/user/user_email?email=${user.email}`
-                );
-                setUserData(userResponse.data.result);
+	useEffect(() => {
+		async function fetchData() {
+			try {
+				const userResponse = await axios.get(
+					`https://e-commerce-grupo03.onrender.com/user/user_email?email=${user.email}`,
+				);
+				setUserData(userResponse.data.result);
 
-                if (userResponse.data.result.userId) {
-                    const cartResponse = await axios.get(
-                        `https://e-commerce-grupo03.onrender.com/cart/getShoppingCart?id=${userResponse.data.result.userId}`
-                    );
-                    console.log(cartResponse.data)
+				if (userResponse.data.result.userId) {
+					const cartResponse = await axios.get(
+						`https://e-commerce-grupo03.onrender.com/cart/getShoppingCart?id=${userResponse.data.result.userId}`,
+					);
+					console.log(cartResponse.data);
 
-                    const activeCart = cartResponse.data.result.find((cart) => cart.isActive === true);
-                    if (activeCart && activeCart.articles) {
-                        setCartItems(activeCart.articles);
-                        setCartResponse(activeCart);
-                    }
-                }
-            } catch (error) {
-                alert('Ha ocurrido un error: ' + error.message);
-            }
-        }
+					const activeCart = cartResponse.data.result.find(
+						(cart) => cart.isActive === true,
+					);
+					if (activeCart && activeCart.articles) {
+						setCartItems(activeCart.articles);
+						setCartResponse(activeCart);
+					}
+				}
+			} catch (error) {
+				alert('Ha ocurrido un error: ' + error.message);
+			}
+		}
 
-        fetchData();
-    }, [user.email]);
+		fetchData();
+	}, [user.email]);
 
-    const handleRemoveButton = async (value) => {
-        try {
-            if (cartResponse) {
-                const cartId = cartResponse.cartId;
-                const response = await axios.get(
-                    `https://e-commerce-grupo03.onrender.com/cart/remove_article_cart?cartid=${cartId}&articleid=${value.articleId}`
-                );
-                alert("Producto eliminado con éxito");
-                if (response) window.location.reload();
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    };
+	const handleRemoveButton = async (value) => {
+		try {
+			if (cartResponse) {
+				const cartId = cartResponse.cartId;
+				const response = await axios.get(
+					`https://e-commerce-grupo03.onrender.com/cart/remove_article_cart?cartid=${cartId}&articleid=${value.articleId}`,
+				);
+				alert('Producto eliminado con éxito');
+				if (response) window.location.reload();
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-    const handleCleanButton = async () => {
-        try {
-            if (cartResponse) {
-                const cartId = cartResponse.cartId;
-                const response = await axios.get(
-                    `https://e-commerce-grupo03.onrender.com/cart/cleanShoppingCart?cartId=${cartId}`
-                );
-                alert("Carrito limpiado con éxito");
-                if (response) window.location.reload();
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    };
+	const handleCleanButton = async () => {
+		try {
+			if (cartResponse) {
+				const cartId = cartResponse.cartId;
+				const response = await axios.get(
+					`https://e-commerce-grupo03.onrender.com/cart/cleanShoppingCart?cartId=${cartId}`,
+				);
+				alert('Carrito limpiado con éxito');
+				if (response) window.location.reload();
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-    const handleBuyCart = async () => {
-        try {
-            if (cartResponse) {
-                navigate('/checkout', { state: { cartItems, cartSubtotal: cartResponse.cartSubtotal, cartId: cartResponse.cartId } });
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    };
+	const handleBuyCart = async () => {
+		try {
+			if (cartResponse) {
+				navigate('/checkout', {
+					state: {
+						cartItems,
+						cartSubtotal: cartResponse.cartSubtotal,
+						cartId: cartResponse.cartId,
+					},
+				});
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-    if (cartItems.length > 0) {
-        return (
-            <div>
-                <button onClick={() => handleCleanButton()}>Limpiar carrito</button>
-                {cartItems.map((product) => (
-                    <div key={product.articleId}>
-                        <img src={product.articleImage} alt={product.articleName} />
-                        <h2>{product.articleName}</h2>
-                        <p>Precio: ${product.articlePrice}</p>
-                        <p>Cantidad: {product.Cart_Articule.articleQuantity}</p>
-                        <button onClick={() => handleRemoveButton(product)}>Eliminar producto</button>
-                    </div>
-                ))}
-                <button onClick={() => handleBuyCart()}>Comprar carrito</button>
-            </div>
-        );
-    } else {
-        return (
-            <div>
-                <h2>No tiene ningún artículo en el carrito.</h2>
-            </div>
-        );
-    }
+	if (cartItems.length > 0) {
+		return (
+			<div className='Shooping-Cart'>
+				<button onClick={() => handleCleanButton()}>Clean Cart</button>
+				<p className='Shopping-Cart-title'>Shopping Cart</p>
+				{cartItems.map((product) => (
+					<div key={product.articleId} className='Cart-Item'>
+						<img src={product.articleImage} alt={product.articleName} />
+						<div className='Cart-Item-Details'>
+							<h2>{product.articleName}</h2>
+							<p>Price: ${product.articlePrice}</p>
+							<p>Quantity: {product.Cart_Articule.articleQuantity}</p>
+						</div>
+						<button
+							onClick={() => handleRemoveButton(product)}
+							className='Remove-Button'
+						>
+							Remove item
+						</button>
+					</div>
+				))}
+				<div className='Cart-Totals'>
+					<p>Total: ${cartResponse.cartSubtotal}</p>
+				</div>
+				<div className='Cart-Checkout'>
+					<button onClick={() => handleBuyCart()}>Checkout</button>
+				</div>
+			</div>
+		);
+	} else {
+		return (
+			<div>
+				<h2>No tiene ningún artículo en el carrito.</h2>
+			</div>
+		);
+	}
 }
