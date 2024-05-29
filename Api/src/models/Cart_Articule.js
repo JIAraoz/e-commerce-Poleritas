694e99ce82d72e4aabc65060918031b9119a717f
aljodavid10 <aljodavid100@gmail.com>
2024-05-29 16:18:02 -0500

    fix models delete sizes

--- Api/src/models/Cart_Articule.js
+++ Api/src/models/Cart_Articule.js
@@ -6,41 +6,6 @@ function Cart_Articule (sequelize){
             type: DataTypes.INTEGER,
             allowNull: false,
             defaultValue: 0
-        },
-        XS: {
-            type: DataTypes.INTEGER,
-            allowNull: true,
-            defaultValue: 0
-        },
-        S: {
-            type: DataTypes.INTEGER,
-            allowNull: true,
-            defaultValue: 0
-        },
-        L: {
-            type: DataTypes.INTEGER,
-            allowNull: true,
-            defaultValue: 0
-        },
-        M: {
-            type: DataTypes.INTEGER,
-            allowNull: true,
-            defaultValue: 0
-        },
-        XL: {
-            type: DataTypes.INTEGER,
-            allowNull: true,
-            defaultValue: 0
-        },
-        XXL: {
-            type: DataTypes.INTEGER,
-            allowNull: true,
-            defaultValue: 0
-        },
-        XXXL: {
-            type: DataTypes.INTEGER,
-            allowNull: true,
-            defaultValue: 0
         }
     })
 }
