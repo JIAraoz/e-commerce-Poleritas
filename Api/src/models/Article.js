1d0c1bc77c3cab8cb4bc6e745e27f577c917a7d7
Nachvander <nachvander@gmail.com>
2024-05-29 19:31:17 -0300

    delete05

--- Api/src/models/Article.js
+++ Api/src/models/Article.js
@@ -1,4 +1,4 @@
-/* const { DataTypes } = require("sequelize");
+const { DataTypes } = require("sequelize");
 
 function article(sequelize) {
   sequelize.define("article", {
@@ -57,63 +57,4 @@ function article(sequelize) {
 }
 
 module.exports = article;
- */
-const { DataTypes } = require("sequelize");
 
-function article(sequelize) {
-  sequelize.define("article", {
-    articleId: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      allowNull: false,
-      primaryKey: true,
-    },
-    articleName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    articleDescription: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    articlePrice: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
-    },
-    articleImage: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    articleStock: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    articleS: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        defaultValue: 0
-    },
-    articleM: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        defaultValue: 0
-    },
-    articleL: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        defaultValue: 0
-    },
-    articleXL: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        defaultValue: 0
-    },
-    articleXXL: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        defaultValue: 0
-    }
-  });
-}
-
-module.exports = article;
