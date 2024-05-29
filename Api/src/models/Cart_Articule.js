9567b4a0378f2cccd92d8ba934458501819eb601
Nachvander <nachvander@gmail.com>
2024-05-29 16:36:56 -0300

    fix-test05

--- Api/src/models/Cart_Articule.js
+++ Api/src/models/Cart_Articule.js
@@ -9,37 +9,37 @@ function Cart_Articule (sequelize){
         },
         XS: {
             type: DataTypes.INTEGER,
-            allowNull: false,
+            allowNull: true,
             defaultValue: 0
         },
         S: {
             type: DataTypes.INTEGER,
-            allowNull: false,
+            allowNull: true,
             defaultValue: 0
         },
         L: {
             type: DataTypes.INTEGER,
-            allowNull: false,
+            allowNull: true,
             defaultValue: 0
         },
         M: {
             type: DataTypes.INTEGER,
-            allowNull: false,
+            allowNull: true,
             defaultValue: 0
         },
         XL: {
             type: DataTypes.INTEGER,
-            allowNull: false,
+            allowNull: true,
             defaultValue: 0
         },
         XXL: {
             type: DataTypes.INTEGER,
-            allowNull: false,
+            allowNull: true,
             defaultValue: 0
         },
         XXXL: {
             type: DataTypes.INTEGER,
-            allowNull: false,
+            allowNull: true,
             defaultValue: 0
         }
     })
