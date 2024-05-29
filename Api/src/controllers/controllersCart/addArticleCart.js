723c1f7477acddc4631eebf2b002c660de655988
aljodavid10 <aljodavid100@gmail.com>
2024-05-29 13:34:25 -0500

    Feat addArticles sizes

--- Api/src/controllers/controllersCart/addArticleCart.js
+++ Api/src/controllers/controllersCart/addArticleCart.js
@@ -1,39 +1,41 @@
-const { ShoppingCart, Article,Cart_Articule } = require('../../db');
+const { ShoppingCart, Article, Size, Cart_Articule, Article_Size } = require('../../db');
 
 const addArticleCart = async (req, res) => {
     try {
-        const idCart = req.query.cartid;
-        const idArticle = req.query.articleid;
-        const quantity = parseInt(req.query.quantity, 10);
+
+        const { idCart, idArticle, XS, S, M, L, XL, XXL, XXXL } = req.body;
+
+        const quantity = XS + S + M + L + XL + XXL + XXXL;
 
         const cart = await ShoppingCart.findOne({
             where: {
                 cartId: idCart
             },
             include: {
-                model: Article
+                model: Article,
+                include: {
+                    model: Size
+                }
             }
         });
 
         if (!cart) {
-            return res.status(404).json({ message: 'Carrito no encontrado' });
+            return res.status(404).json({ message: 'Cart not found' });
         }
 
         const article = await Article.findByPk(idArticle);
         if (!article) {
-            return res.status(404).json({ message: 'Artículo no encontrado' });
+            return res.status(404).json({ message: 'Article not found' });
         }
         const articleExist=await Cart_Articule.findOne({where:{
             articleArticleId:article.articleId,
             shoppingCartCartId:cart.cartId
         }})
         if(articleExist===null){
-            await cart.addArticle(article, { through: { articleQuantity:quantity } });
+            await cart.addArticle(article, { through: { articleQuantity:quantity, XS, S, M, L, XL, XXL, XXXL } });
             
             const response = await Article.findByPk(article.articleArticleId);
-            console.log(response.dataValues.articlePrice)
             const subtotalAux = response.dataValues.articlePrice * parseInt(quantity);
-            console.log(subtotalAux);
             cart.cartSubtotal += subtotalAux;
            
             
@@ -44,10 +46,14 @@ const addArticleCart = async (req, res) => {
             const subtotalAux = response.dataValues.articlePrice * parseInt(quantity);
             console.log(subtotalAux);
             cart.cartSubtotal += subtotalAux;
-            
-          
-            
             await cart.save();
+            articleExist.XS = XS;
+            articleExist.S = S;
+            articleExist.M = M;
+            articleExist.L = L;
+            articleExist.XL = XL;
+            articleExist.XXL = XXL;
+            articleExist.XXXL = XXXL;
             articleExist.articleQuantity += quantity;
             await articleExist.save();
         }
@@ -57,14 +63,14 @@ const addArticleCart = async (req, res) => {
         });
 
         res.status(200).json({
-            message: 'Artículo agregado al carrito con éxito',
+            message: 'Article successfully added to cart',
             cart: updatedCart
         });
 
        
     } catch (error) {
         console.error(error);
-        return res.status(500).json({ message: 'Error interno del servidor' });
+        return res.status(500).json({ message: 'Internal server error' });
     }
 }
 
