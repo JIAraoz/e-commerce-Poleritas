2c60172a50be95efb6f9e876fc655edaa446dfe0
MateoGiampietro <mateo.giampietro.10@gmail.com>
2024-05-30 15:15:05 -0300

    add edit profile

--- Api/src/routes/detailRoutes/routerUser.js
+++ Api/src/routes/detailRoutes/routerUser.js
@@ -3,9 +3,11 @@ const routerUser = require("express").Router();
 const getUserByEmail = require("../../controllers/controllersUser/getUserByEmail");
 const getUser = require("../../controllers/controllersUser/getUsers");
 const postUser=require('../../controllers/controllersUser/postUser')
+const editUser = require('../../controllers/controllersUser/editUser');
 
 routerUser.get('/user_email', getUserByEmail);
 routerUser.get('/list-users', getUser);
 routerUser.post('/postUser',postUser);
+routerUser.put('/update', editUser);
 
 module.exports = routerUser;
