68e4fe8cd13bc5603036848d785cd0fc9f6c737b
SAldanaNizama <140188751+SAldanaNizama@users.noreply.github.com>
2024-06-02 15:32:18 -0500

    fix-reviews

--- Api/src/controllers/controllersReview/postReviews.js
+++ Api/src/controllers/controllersReview/postReviews.js
@@ -1,26 +1,37 @@
+const { Review, User, ShoppingCart } = require('../../db');
+
 const postReviews = async (req, res) => {
-  const { reviewRating, reviewDescription } = req.body;
+  const { userId, reviewRating, reviewDescription } = req.body;
 
   try {
-    const user = req.user; // Esto asume que tienes un middleware que agrega el usuario a la solicitud
+    // Buscar el usuario y sus carritos desactivados
 
-    // Si el usuario no está disponible o no tiene carritos desactivados, no puede crear una reseña
+    const user = await User.findOne({
+      where: { userId },
+      include: {
+        model: ShoppingCart,
+        where: { isActive: false }
+      }
+    });
+
+    // Si el usuario no tiene carritos desactivados, no puede crear una reseña
     if (!user || user.shoppingCarts.length === 0) {
       return res.status(400).json({ message: 'El usuario no ha realizado ninguna compra.' });
     }
 
     // Verificar si el usuario ya tiene una reseña
-    const existingReview = await Review.findOne({ where: { userId: user.userId } });
+    const existingReview = await Review.findOne({ where: { userId } });
     if (existingReview) {
       return res.status(400).json({ message: 'El usuario ya tiene una reseña. Use PUT para actualizar.' });
     }
 
-    // Crear la nueva reseña asociada al usuario
-    const review = await user.createReview({ reviewRating, reviewDescription });
+    // Crear la nueva reseña
+    const review = await Review.create({ reviewRating, reviewDescription });
+    await user.addReview(review)
     return res.status(201).json(review);
   } catch (error) {
     res.status(500).json({ message: 'Error al crear la reseña.', error: error.message });
   }
 };
 
-module.exports=postReviews
+module.exports = postReviews;
