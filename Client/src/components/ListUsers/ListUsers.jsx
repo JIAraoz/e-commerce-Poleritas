00fc7cac8a5763e03a33bed6a2fbf190aad8fe11
Nachvander <nachvander@gmail.com>
2024-06-04 16:34:49 -0300

    searchbar

--- Client/src/components/ListUsers/ListUsers.jsx
+++ Client/src/components/ListUsers/ListUsers.jsx
@@ -1,35 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import{ useState} from 'react';
 import UserCard from '../Card/CardUsers';
-
+import UsersSearchBar from '../SearchBar/UsersSearchBar';
 const ListUsers = () => {
 	const [users, setUsers] = useState([]);
-
-	useEffect(() => {
-		axios
-			.get('https://e-commerce-grupo03.onrender.com/user/list-users')
-			.then((response) => {
-				if (response.data && Array.isArray(response.data.result)) {
-					setUsers(response.data.result);
-				} else {
-					console.error(
-						'La API no devolvió un array en la propiedad result: ',
-						response.data,
-					);
-				}
-			})
-			.catch((error) => {
-				console.error('Hubo un error al obtener los usuarios: ', error);
-			});
-	}, []);
 
 	return (
 		<div>
 			<h1>Lista de Usuarios</h1>
+			<UsersSearchBar setUsers={setUsers}/>
 			{Array.isArray(users) &&
 				users.map((user, index) => {
-					console.log(user);
-					return <UserCard key={index} user={user} />;
+					
+					return <UserCard key={index} user={user} setUsers={setUsers}/>;
 				})}
 		</div>
 	);
