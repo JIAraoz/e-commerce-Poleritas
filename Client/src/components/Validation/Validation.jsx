dc65aa94fd817a81b50498712a16a84a1f7fa166
Brayan Holguin <116777008+Brayan8Holguin@users.noreply.github.com>
2024-06-05 03:24:31 -0600

    style-form
    la estilicacion en form quedo mejor a lo pedido, aun falta mediaquerys

--- Client/src/components/Validation/Validation.jsx
+++ Client/src/components/Validation/Validation.jsx
@@ -4,7 +4,7 @@ export default function Validation(articleData, errors, setErrors) {
 	for (const key in articleData) {
 		switch (key) {
 			case 'articleName':
-				if (!articleData.articleName.trim()) {
+				if (!articleData.articleName.trim) {
 					errorsCopy.articleName = '*El nombre es obligatorio';
 				} else if (articleData.articleName.length > 40) {
 					errorsCopy.articleName =
@@ -14,68 +14,93 @@ export default function Validation(articleData, errors, setErrors) {
 				}
 				break;
 			case 'articleImage':
-				if (!articleData.articleImage.trim()) {
+				if (!articleData.articleImage.trim) {
 					errorsCopy.articleImage = '*La imagen es obligatoria';
 				} else {
 					delete errorsCopy.articleImage;
 				}
 				break;
 			case 'articlePrice':
-				if (
-					!articleData.articlePrice.trim() ||
-					isNaN(articleData.articlePrice)
-				) {
+				if (!articleData.articlePrice.trim || isNaN(articleData.articlePrice)) {
 					errorsCopy.articlePrice = '*El precio debe ser un número';
+				} else if (Number(articleData.articlePrice) < 0) {
+					errorsCopy.articlePrice =
+						'*El precio no puede ser un número negativo';
 				} else {
 					delete errorsCopy.articlePrice;
 				}
 				break;
 
 			case 'articleDescription':
-				if (!articleData.articleDescription.trim()) {
+				if (!articleData.articleDescription.trim) {
 					errorsCopy.articleDescription = '*La descripción es obligatoria';
 				} else {
 					delete errorsCopy.articleDescription;
 				}
 				break;
 			case 'Category':
-				if (!articleData.Category.trim()) {
+				if (!articleData.Category.trim) {
 					errorsCopy.Category = '*La categoría es obligatoria';
 				} else {
 					delete errorsCopy.Category;
 				}
 				break;
 			case 'articleS':
-				if (!articleData.articleS.trim()) {
+				if (!articleData.articleS.trim) {
 					errorsCopy.articleS = '*El stock para S es obligatorio';
+				} else if (isNaN(articleData.articleS)) {
+					errorsCopy.articleS = '*El stock para S debe ser un número';
+				} else if (Number(articleData.articleS) < 0) {
+					errorsCopy.articleS =
+						'*El stock para S no puede ser un número negativo';
 				} else {
 					delete errorsCopy.articleS;
 				}
 				break;
 			case 'articleM':
-				if (!articleData.articleM.trim()) {
+				if (!articleData.articleM.trim) {
 					errorsCopy.articleM = '*El stock para M es obligatorio';
+				} else if (isNaN(articleData.articleM)) {
+					errorsCopy.articleM = '*El stock para M debe ser un número';
+				} else if (Number(articleData.articleM) < 0) {
+					errorsCopy.articleM =
+						'*El stock para M no puede ser un número negativo';
 				} else {
 					delete errorsCopy.articleM;
 				}
 				break;
 			case 'articleL':
-				if (!articleData.articleL.trim()) {
+				if (!articleData.articleL.trim) {
 					errorsCopy.articleL = '*El stock para L es obligatorio';
+				} else if (isNaN(articleData.articleL)) {
+					errorsCopy.articleL = '*El stock para L debe ser un número';
+				} else if (Number(articleData.articleL) < 0) {
+					errorsCopy.articleL =
+						'*El stock para L no puede ser un número negativo';
 				} else {
 					delete errorsCopy.articleL;
 				}
 				break;
 			case 'articleXL':
-				if (!articleData.articleXL.trim()) {
+				if (!articleData.articleXL.trim) {
 					errorsCopy.articleXL = '*El stock para XL es obligatorio';
+				} else if (isNaN(articleData.articleXL)) {
+					errorsCopy.articleXL = '*El stock para XL debe ser un número';
+				} else if (Number(articleData.articleXL) < 0) {
+					errorsCopy.articleXL =
+						'*El stock para XL no puede ser un número negativo';
 				} else {
 					delete errorsCopy.articleXL;
 				}
 				break;
 			case 'articleXXL':
-				if (!articleData.articleXXL.trim()) {
+				if (!articleData.articleXXL.trim) {
 					errorsCopy.articleXXL = '*El stock para XXL es obligatorio';
+				} else if (isNaN(articleData.articleXXL)) {
+					errorsCopy.articleXXL = '*El stock para XXL debe ser un número';
+				} else if (Number(articleData.articleXXL) < 0) {
+					errorsCopy.articleXXL =
+						'*El stock para XXL no puede ser un número negativo';
 				} else {
 					delete errorsCopy.articleXXL;
 				}
